5f7b765a8d151a5aaf415009a3c0dbce95e206d9
yeonwha0704 <ppyyhh3285@gmail.com>
2024-08-07 18:07:02 +0900

    add ch03 04 05 240807

---
+++ ch03/js/calc.js
@@ -0,0 +1,13 @@
+// 라이브러리 파일
+function plus(a, b){
+    return a + b;
+}
+function minus(a, b){
+    return a - b;
+}
+function multi(a, b){
+    return a * b;
+}
+function div(a, b){
+    return a / b;
+}
